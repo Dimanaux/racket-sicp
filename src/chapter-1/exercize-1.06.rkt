a626096e3c4e846c84a0e7d78353ae3b7d57b7aa
Dmitry Barskov <barskoidim@gmail.com>
2018-08-24 09:15:42 +0300

    exercize 1.1.6 solved

---
+++ src/chapter-1/exercize-1.06.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+(define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+    (average guess (/ x guess)))
+
+(define (average x y)
+    (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+    (sqrt-iter 1.0 x))
+
+(define (new-if predicate then-clause else-clause)
+    (cond (predicate then-clause)
+          (else else-clause)))
+
+(define (sqrt-iter guess x)
+    (new-if (good-enough? guess x)
+            guess
+            (sqrt-iter (improve guess x) x)))
+
+; the last definition with new-if inside
+; will loop forever, since new-if is a
+; procedure, and it's arguments is being
+; evaluated before procedure call.
+; so even if guess is good enough,
+; next sqrt-iter call will be invoked. 
+
