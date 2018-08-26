7e1fe69def72e677267adcd491295c566bebaf9e
Dmitry Barskov <barskoidim@gmail.com>
2018-08-26 07:52:45 +0300

    typo fix in test-1.1.2 path

--- test/chapter-1/exercise-1.02-test.rkt
+++ test/chapter-1/exercise-1.02-test.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (require rackunit
-         "../../src/chapter-1/exersize-1.02.rkt")
+         "../../src/chapter-1/exercise-1.02.rkt")
 
 (check-equal? prefix (/ -37 150) "prefix form expression")
 
