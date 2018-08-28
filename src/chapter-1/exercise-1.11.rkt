3e6514819cf4753b0e6d5405accde6b33e7f4175
Dmitry Barskov <barskoidim@gmail.com>
2018-08-28 18:12:05 +0300

    add f-iterative for solution of exercise 1.11

--- src/chapter-1/exercise-1.11.rkt
+++ src/chapter-1/exercise-1.11.rkt
@@ -10,4 +10,17 @@
                 (f (- n 3))))))
 
 
+(define (f-iterative n)
+    (f-iter 0 1 2 n))
+
+
+(define (f-iter a b c count)
+    (if (= count 0)
+        c
+        (f-iter
+            (+ a b c)
+            (+ b c)
+            a
+            (- count 1))))
+
 
