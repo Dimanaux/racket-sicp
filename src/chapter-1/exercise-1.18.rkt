5d6343656d3196a95c00931d4c14b5f2559cffa6
Dmitry Barskov <barskoidim@gmail.com>
2018-09-01 07:10:58 +0300

    solve exercise 1.18

--- src/chapter-1/exercise-1.18.rkt
+++ src/chapter-1/exercise-1.18.rkt
@@ -13,11 +13,30 @@
 
 
 (define (fast-* a b)
-    (define (iter count product)
+    (define (iter a b product)
         (cond
-            ((= count 0) 0)
-            ((= count 1) product)
-            ((even? count) (iter (halve count) (double product)))
-            (else (iter (- count 1) (+ product b)))))
-    (iter a 1))
+            ((= a 0) product)
+            ((even? a) (iter (halve a) (double b) product))
+            (else (iter (- a 1) b (+ product b)))))
+    (iter a b 0))
+
+
+(define (test-*)
+    (define (assert x y)
+        (if (= (fast-* x y) (* x y))
+            (println "OK")
+            (println (format "OPPS ~a * ~a != ~a" x y (fast-* x y)))))
+    (assert 2 5)
+    (assert 1 8)
+    (assert 5 2)
+    (assert 8 1)
+    (assert 0 4)
+    (assert 4 0)
+    (assert 3 7)
+    (assert 7 3)
+    (assert 10 2)
+    (assert 2 10)
+    (assert 3 8))
+
+(test-*)
 
