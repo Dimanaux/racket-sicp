a50e5e91112922a1e71ba5e0c63f9bda503edca7
Dmitry Barskov <barskoidim@gmail.com>
2018-09-01 07:12:12 +0300

    remove tests from 1.18 solution

--- src/chapter-1/exercise-1.18.rkt
+++ src/chapter-1/exercise-1.18.rkt
@@ -20,23 +20,3 @@
             (else (iter (- a 1) b (+ product b)))))
     (iter a b 0))
 
-
-(define (test-*)
-    (define (assert x y)
-        (if (= (fast-* x y) (* x y))
-            (println "OK")
-            (println (format "OPPS ~a * ~a != ~a" x y (fast-* x y)))))
-    (assert 2 5)
-    (assert 1 8)
-    (assert 5 2)
-    (assert 8 1)
-    (assert 0 4)
-    (assert 4 0)
-    (assert 3 7)
-    (assert 7 3)
-    (assert 10 2)
-    (assert 2 10)
-    (assert 3 8))
-
-(test-*)
-
