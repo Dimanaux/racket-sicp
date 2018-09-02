23ad98d1b1d3d02e87b341e3b36925940ac57748
Dmitry Barskov <barskoidim@gmail.com>
2018-09-02 09:05:33 +0300

    add tests for 1.19 solution

---
+++ test/chapter-1/exercise-1.19-test.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+
+(require
+    rackunit
+    "../../src/chapter-1/exercise-1.19.rkt")
+
+
+(check-equal? (fib 0) 0)
+(check-equal? (fib 1) 1)
+(check-equal? (fib 2) 1)
+(check-equal? (fib 3) 2)
+(check-equal? (fib 4) 3)
+(check-equal? (fib 5) 5)
+(check-equal? (fib 6) 8)
+(check-equal? (fib 7) 13)
+(check-equal? (fib 8) 21)
+(check-equal? (fib 9) 30)
+
