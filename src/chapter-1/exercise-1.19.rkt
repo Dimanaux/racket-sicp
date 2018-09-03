a138c91fe3aebab21c6963d1f2b8b12d5ab7a751
Dmitry Barskov <barskoidim@gmail.com>
2018-09-03 07:49:07 +0300

    solve exercise 1.19

---
+++ src/chapter-1/exercise-1.19.rkt
@@ -0,0 +1,30 @@
+#lang racket
+
+
+(provide fib)
+
+
+(define (fib n)
+    (fib-iter 1 0 0 1 n))
+
+
+(define (fib-iter a b p q count)
+    (cond
+        ((= count 0) b)
+        ((even? count)
+            (fib-iter
+                a
+                b
+                (+ (sqr p) (sqr q))
+                (+ (* 2 p q) (sqr q))
+                (/ count 2)))
+        (else
+            (fib-iter
+                (+ (* b q) (* a q) (* a p))
+                (+ (* b p) (* a q))
+                p
+                q
+                (- count 1)))))
+
+
+                        
