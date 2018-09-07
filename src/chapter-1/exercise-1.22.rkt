dc388aeff3a54abafcef310d0688933f7b4c3b97
Dmitry Barskov <barskoidim@gmail.com>
2018-09-07 06:25:39 +0300

    add 1.22 solution file with description

--- src/chapter-1/exercise-1.22.rkt
+++ src/chapter-1/exercise-1.22.rkt
@@ -1,5 +1,17 @@
 #lang racket
 
+; TASK
+; Write procedure search-for-primes
+; that tests for primeness all odd
+; number in range.
+; Find 3 smallest prime numbers
+; greater than 1 000,
+; greater than 10 000,
+; greater than 100 000,
+; greater than 1 000 000.
+; How many time does it takes?
+; Does this algorithm takes Tetha(sqrt(n))?
+
 
 (define (timed-prime-test n)
     (newline)
