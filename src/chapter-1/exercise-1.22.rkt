7e56657613fb2cd57b161b81c214fca0dfd2069d
Dmitry Barskov <barskoidim@gmail.com>
2018-09-08 16:00:25 +0300

    move procedures to lib

--- src/chapter-1/exercise-1.22.rkt
+++ src/chapter-1/exercise-1.22.rkt
@@ -1,30 +1,7 @@
 #lang racket
 
 
-(define (expmod base exp m)
-    (cond
-        ((= exp 0) 1)
-        ((even? exp) (remainder (sqr (expmod base (/ exp 2) m)) m))
-        (else (remainder (* base (expmod base (- exp 1) m)) m))))
-
-
-(define (fermat-test n)
-    (define (try-it a)
-        (= (expmod a n n) a))
-    (try-it (+ 1 (random (- n 1)))))
-
-
-(define (fast-prime? n times)
-    (cond
-        ((= times 0) true)
-        ((fermat-test n) (fast-prime? n (- times 1)))
-        (else #f)))
-
-
-(define (prime? n)
-    (fast-prime? n 15))
-
-
+(require "../lib/prime.rkt")
 
 ; TASK
 ; Write procedure search-for-primes
