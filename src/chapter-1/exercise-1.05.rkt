60357fc9b1844ad50435916b09ff2a1293512c7c
Dmitry <barskoidim@gmail.com>
2019-02-09 11:06:17 +0300

    Update exercise-1.05.rkt

--- src/chapter-1/exercise-1.05.rkt
+++ src/chapter-1/exercise-1.05.rkt
@@ -1,5 +1,11 @@
 #lang racket
 
+; Exercise 1.5.
+; Ben Bitdiddle has invented a test to determine whether
+; the interpreter he is faced with is using
+; applicative-order evaluation or normal-order evaluation.
+; He defines the following two procedures:
+
 (define (p) (p))
 
 (define (test x y)
