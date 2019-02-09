43e9606e76db17eaabf0998d60dec3bdf578f038
Dmitry <barskoidim@gmail.com>
2019-02-09 11:01:42 +0300

    Add 1.2 exercise description

--- src/chapter-1/exercise-1.02.rkt
+++ src/chapter-1/exercise-1.02.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+; Exercise 1.2.
+; Translate the following expression into prefix form
 ; 5 + 4 + (2 - (3 - (6 + 4/5)))
 ; _____________________________
 ;       3 (6 - 2) (2 - 7)
@@ -9,4 +11,3 @@
 (define prefix
     (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
        (* 3 (- 6 2) (- 2 7))))
-
