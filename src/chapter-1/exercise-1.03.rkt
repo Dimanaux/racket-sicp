8b0d11a6c1e47f2146dead1f0004cdb1d911a6d9
Dmitry <barskoidim@gmail.com>
2019-02-09 11:03:39 +0300

    Add exercise 1.3 description

--- src/chapter-1/exercise-1.03.rkt
+++ src/chapter-1/exercise-1.03.rkt
@@ -1,9 +1,13 @@
 #lang racket
 
+; Exercise 1.3.
+; Define a procedure that takes three numbers as arguments
+; and returns the sum of the squares of the two larger
+; numbers.
+
 (provide sum-of-squares-of-two-max)
 
 (define (sum-of-squares-of-two-max x y z)
     (if (> x y)
         (+ (sqr x) (sqr (max y z)))
         (+ (sqr y) (sqr (max x z)))))
-
