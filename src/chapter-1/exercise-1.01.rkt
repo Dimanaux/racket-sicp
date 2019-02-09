332239ff92129e070e5dcb57b587a4689512df89
Dmitry <barskoidim@gmail.com>
2019-02-09 11:00:07 +0300

    Add 1.1 problem description

--- src/chapter-1/exercise-1.01.rkt
+++ src/chapter-1/exercise-1.01.rkt
@@ -1,5 +1,11 @@
 #lang racket
 
+; Exercise 1.1.
+; Below is a sequence of expressions. What is the result
+; printed by the interpreter in response to each expression?
+; Assume that the sequence is to be evaluated in the order
+; in which it is presented.
+
 10
 ; > 10
 
@@ -45,5 +51,3 @@
          (else -1))
    (+ a 1))
 ; > 16
-
-
