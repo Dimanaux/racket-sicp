8197bc147c60993be5f45bdcd3d85f818bc79f0a
Dmitry <barskoidim@gmail.com>
2019-02-10 18:24:38 +0300

    Exercise 1.6: add description

--- src/chapter-1/exercise-1.06.rkt
+++ src/chapter-1/exercise-1.06.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+; Exercise 1.6
+
 (define (sqrt-iter guess x)
     (if (good-enough? guess x)
         guess
@@ -17,15 +19,34 @@
 (define (sqrt x)
     (sqrt-iter 1.0 x))
 
-; new if procedure
+; Alyssa P. Hacker doesn't see why if needs to be provided
+; as a special form. "Why can't I just define it as an
+; ordinary procedure in terms of cond?" she asks. Alyssa's
+; friend Eva Lu Ator claims this can indeed be done, and
+; she defines a new version of if:
+
 (define (new-if predicate then-clause else-clause)
     (cond (predicate then-clause)
           (else else-clause)))
 
+; Eva demonstrates the program for Alyssa:
+;(new-if (= 2 3) 0 5)
+; > 5
+
+; (new-if (= 1 1) 0 5)
+; > 0
+
+; Delighted, Alyssa uses new-if to rewrite the
+; square-root program:
+
 (define (sqrt-iter guess x)
-    (new-if (good-enough? guess x)
-            guess
-            (sqrt-iter (improve guess x) x)))
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+
+; What happens when Alyssa attempts to use this to compute
+; square roots? Explain.
 
 ; the last definition with new-if inside
 ; will loop forever, since new-if is a
