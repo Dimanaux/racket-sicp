e1723e0d670e462bd886a4223d08bde63f13db90
Dmitry <barskoidim@gmail.com>
2019-02-11 08:56:51 +0300

    Update exercise-1.12.rkt

--- src/chapter-1/exercise-1.12.rkt
+++ src/chapter-1/exercise-1.12.rkt
@@ -2,11 +2,9 @@
 
 (provide pascal-triangle)
 
-(define (pascal-triangle row index)
+(define (pascal-triangle row ind)
     (cond
-        (((or (= index 1) (= row index))
-            1))
-        (else
-            (+
-                (pascal-triangle (- row 1) index)
-                (pascal-triangle (- row 1) (- index 1))))))
+        ((= ind 1)   1)
+        ((= row ind) 1)
+        (else (+ (pascal-triangle (- row 1) ind)
+                 (pascal-triangle (- row 1) (- ind 1))))))
