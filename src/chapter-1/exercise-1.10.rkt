9fe41784f83d075a7ed22d628e09387bd548214d
Dmitry <barskoidim@gmail.com>
2019-02-16 18:25:37 +0300

    Add 1.10 exercise description

--- src/chapter-1/exercise-1.10.rkt
+++ src/chapter-1/exercise-1.10.rkt
@@ -1,12 +1,28 @@
 #lang racket
 
-
+; Exercise 1.10.
+; The following procedure computes a mathematical function
+; called Ackermann's function.
 (define (A x y)
-    (cond ((= y 0) 0)
-          ((= x 0) (* 2 y))
-          ((= y 1) 2)
-          (else
-              (A (- x 1) (A x (- y 1))))))
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+; What are the values of the following expressions?
+; (A 1 10)
+; (A 2 4)
+; (A 3 3)
+; Consider the following procedures, where A
+; is the procedure defined above:
+; (define (f n) (A 0 n))
+; (define (g n) (A 1 n))
+; (define (h n) (A 2 n))
+; (define (k n) (* 5 n n))
+; Give concise mathematical definitions for the functions
+; computed by the procedures f, g, and h for positive
+; integer values of n. For example, (k n) computes 5n^2.
+; ------------------------
 
 (A 1 10)
 ; (A 1 10)
@@ -30,7 +46,6 @@
 ; (A 0 512)
 ; (1024)
 ; > 1024
-
 
 (A 2 4)
 ; (A 2 4)
@@ -64,7 +79,6 @@
 ; (65536)
 ; > 65536
 
-
 (A 3 3)
 ; (A 3 3)
 ; (A 2 (A 3 2))
@@ -89,4 +103,3 @@
 
 (define (k n) (* 5 n n))
 ; 5n^2
-
