3fc796cc25c16a35fca3aad7ae531b601e6d4eb5
cosmoskey <barskoidim@gmail.com>
2019-02-28 07:38:32 +0300

    add description 1.11, change it according to task. Now tests are invalid

--- src/chapter-1/exercise-1.11.rkt
+++ src/chapter-1/exercise-1.11.rkt
@@ -1,5 +1,12 @@
 #lang racket
 
+; Exercise 1.11.
+; A function f is defined by the rule that f(n) = n if n<3
+; and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n>=3.
+; Write a procedure that computes f by means of a
+; recursive process.
+; Write a procedure that computes f by means of an
+; iterative process.
 
 (provide f-recursive
          f-iterative)
@@ -9,9 +16,9 @@
     (cond ((< n 3) n)
         (else
             (+
-                (f-recursive (- n 1))
-                (f-recursive (- n 2))
-                (f-recursive (- n 3))))))
+                (* 1 (f-recursive (- n 1)))
+                (* 2 (f-recursive (- n 2)))
+                (* 3 (f-recursive (- n 3)))))))
 
 
 (define (f-iterative n)
@@ -24,7 +31,5 @@
         (f-iter
             b
             c
-            (+ a b c)
+            (+ (* 3 a) (* 2 b) c)
             (- count 1))))
-
-
