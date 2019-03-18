617505b1f281e887bc935efbdd1b18a3b358526c
Dmitry Barskov <barskoidim@gmail.com>
2019-03-18 09:13:21 +0300

    add description for 1.18

--- src/chapter-1/exercise-1.18.rkt
+++ src/chapter-1/exercise-1.18.rkt
@@ -1,16 +1,18 @@
 #lang racket
 
+; Exercise 1.18.
+; Using the results of exercises 1.16 and 1.17, devise a
+; procedure that generates an iterative process for
+; multiplying two integers in terms of adding, doubling,
+; and halving and uses a logarithmic number of steps.
 
 (provide fast-*)
-
 
 (define (double x)
     (+ x x))
 
-
 (define (halve x)
     (/ x 2))
-
 
 (define (fast-* a b)
     (define (iter a b product)
@@ -19,4 +21,3 @@
             ((even? a) (iter (halve a) (double b) product))
             (else (iter (- a 1) b (+ product b)))))
     (iter a b 0))
-
