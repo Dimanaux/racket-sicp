2370661e0decfe55286b271524a873c2cf920329
Dmitry Barskov <barskoidim@gmail.com>
2019-05-15 18:34:36 +0300

    add description to exercise 1.21

--- src/chapter-1/exercise-1.21.rkt
+++ src/chapter-1/exercise-1.21.rkt
@@ -1,9 +1,11 @@
 #lang racket
 
+; Exercise 1.21.
+; Use the smallest-divisor procedure to find the smallest
+; divisor of each of the following numbers: 199, 1999, 19999
 
 (define (smallest-divisor n)
     (find-divisor n 2))
-
 
 (define (find-divisor n test-divisor)
     (cond
@@ -11,17 +13,13 @@
         ((divides? test-divisor n) test-divisor)
         (else (find-divisor n (+ test-divisor 1)))))
 
-
 (define (divides? a b)
     (= (remainder b a) 0))
-
 
 (define (prime? n)
     (= n (smallest-divisor n)))
 
-
 (println (smallest-divisor 199))
 (println (smallest-divisor 1999))
 (println (smallest-divisor 19999))
-
 
