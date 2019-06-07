7e8446711706d115f361756c8e7553262a3d16e9
Dmitry Barskov <barskoidim@gmail.com>
2019-06-07 11:27:40 +0300

    format lib/fast-prime.rkt

--- src/lib/fast-prime.rkt
+++ src/lib/fast-prime.rkt
@@ -1,15 +1,11 @@
 #lang racket
 
-
 (require "fermat-test.rkt")
-
 
 (define (fast-prime? n times)
     (cond
-        ((= times 0) #t)
+        ((= times 0) true)
         ((fermat-test n) (fast-prime? n (- times 1)))
-        (else #f)))
-
+        (else false)))
 
 (provide fast-prime?)
-
