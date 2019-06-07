dec7b4deff9311350eda4453862cc56b0d0d0241
Dmitry Barskov <barskoidim@gmail.com>
2019-06-07 11:30:08 +0300

    format exercise 1.11 solution

--- src/chapter-1/exercise-1.11.rkt
+++ src/chapter-1/exercise-1.11.rkt
@@ -11,7 +11,6 @@
 (provide f-recursive
          f-iterative)
 
-
 (define (f-recursive n)
     (cond ((< n 3) n)
         (else
@@ -20,10 +19,8 @@
                 (* 2 (f-recursive (- n 2)))
                 (* 3 (f-recursive (- n 3)))))))
 
-
 (define (f-iterative n)
     (f-iter 0 1 2 n))
-
 
 (define (f-iter a b c count)
     (if (= count 0)
