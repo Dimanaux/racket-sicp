2b49074b10d2de8fa5089eebef345b50defac11b
Dmitry Barskov <barskoidim@gmail.com>
2019-06-09 09:56:06 +0300

    reformat code in 1.22-1.24, make it closer to SICP

--- src/chapter-1/exercise-1.23.rkt
+++ src/chapter-1/exercise-1.23.rkt
@@ -1,8 +1,6 @@
 #lang racket
 
-
 (require "../lib/runtime.rkt")
-
 
 ; TASK
 ; find-divisor checked all the numbers 2, 3, 4, 5, 6, 7...
@@ -15,16 +13,13 @@
 (define (prime? x)
     (= (smallest-divisor x) x))
 
-
 (define (next current)
     (cond
         ((= current 2) 3)
         (else (+ current 2))))
 
-
 (define (smallest-divisor n)
     (find-divisor n 2))
-
 
 (define (find-divisor n test-divisor)
     (cond
@@ -32,35 +27,28 @@
         ((divides? test-divisor n) test-divisor)
         (else (find-divisor n (next test-divisor)))))
 
-
 (define (divides? a b)
     (= (remainder b a) 0))
 
-
 (define (timed-prime-test n)
+    (newline)
+    (display n)
     (start-prime-test n (runtime)))
-
 
 (define (start-prime-test n start-time)
     (if (prime? n)
-        (report-prime n (- (runtime) start-time))
-        #f))
+        (report-prime (- (runtime) start-time))
+        false))
 
-
-(define (report-prime number elapsed-time)
-    (display number)
-    (display " is prime. ")
-    (display elapsed-time)
-    (display "ms")
-    (newline))
-
+(define (report-prime elapsed-time)
+    (display " *** ")
+    (display elapsed-time))
 
 (define (search-for-primes number count)
     (cond ((> count 0)
         (cond
             ((timed-prime-test number) (search-for-primes (+ number 1) (- count 1)))
             (else (search-for-primes (+ number 1) count))))))
-
 
 (search-for-primes 1000 3)
 (search-for-primes 10000 3)
@@ -69,4 +57,3 @@
 
 ; Time measures are different for exercises 1.22 and 1.23.
 ; Solution used in 1.23 is 2 times faster than 1.22 one.
-
--- src/chapter-1/exercise-1.24.rkt
+++ src/chapter-1/exercise-1.24.rkt
@@ -1,33 +1,27 @@
 #lang racket
 
-
-(require "../lib/fast-prime.rkt")
-
 ; TASK
 ; modify procedure timed-prime-test so
 ; it will use fast-prime?
 ; check all 12 numbers from 1.22
 
+(require "../lib/fast-prime.rkt")
+
 (define runtime current-inexact-milliseconds)
 
-
 (define (timed-prime-test n)
+    (newline)
+    (display n)
     (start-prime-test n (runtime)))
 
-
 (define (start-prime-test n start-time)
-    (if (fast-prime? n 10)
-        (report-prime n (- (runtime) start-time))
-        #f))
+    (if (fast-prime? n 20)
+        (report-prime (- (runtime) start-time))
+        false))
 
-
-(define (report-prime number elapsed-time)
-    (display number)
-    (display " is prime. ")
-    (display elapsed-time)
-    (display "ms")
-    (newline))
-
+(define (report-prime elapsed-time)
+    (display " *** ")
+    (display elapsed-time))
 
 (define (search-for-primes number count)
     (cond ((> count 0)
@@ -39,5 +33,3 @@
 (search-for-primes 10000 3)
 (search-for-primes 100000 3)
 (search-for-primes 1000000 3)
-
-
