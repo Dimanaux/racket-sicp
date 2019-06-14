9df21275987e8975746a2a1f89655f420dc376ec
Dmitry Barskov <barskoidim@gmail.com>
2019-06-14 13:30:07 +0300

    solve 1.30

---
+++ src/chapter-1/exercise-1.30.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+; Exercise 1.30.
+; The sum procedure above generates a linear recursion.
+; The procedure can be rewritten so that the sum is
+; performed iteratively. Show how to do this by filling
+; in the missing expressions in the following definition:
+
+;;; (define (sum term a next b)
+;;;     (define (iter a result)
+;;;         (if <??>
+;;;             <??>
+;;;             (iter <??> <??>)))
+;;;     (iter <??> <??>))
+
+(provide sum)
+
+(define (sum term a next b)
+    (define (iter a result)
+        (if (> a b)
+            result
+            (iter (next a) (+ (term a) result))))
+    (iter a 0))
